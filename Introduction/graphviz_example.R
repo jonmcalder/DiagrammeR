19807964a816faf24cd6bcc31ed0a10011ea36c8
jonmcalder <jonmcalder@gmail.com>
2016-10-18 02:32:59 +0200

    Fix MANIFEST file, update intro lesson to incorporate basic GraphViz examples and add package dependency to lesson.

---
+++ Introduction/graphviz_example.R
@@ -0,0 +1,24 @@
+graphviz_example <- "
+digraph neato {
+
+graph [layout = neato]
+
+node [shape = circle,
+      style = filled,
+      color = grey,
+      label = '']
+
+node [fillcolor = red]
+a
+
+node [fillcolor = green]
+b c d
+
+node [fillcolor = orange]
+
+edge [color = grey]
+a -> {b c d}
+b -> {e f g h i j}
+c -> {k l m n o p}
+d -> {q r s t u v}
+}"
--- Introduction/initLesson.R
+++ Introduction/initLesson.R
@@ -2,3 +2,14 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
+
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+source(file.path(.get_course_path(), "Diagrammer", "Introduction", 
+                 "graphviz_example.R"))
+
+undirected_xyz <- "grViz('graph {X; Y; Z; X--Y; Y--Z; X--Z;}')"
