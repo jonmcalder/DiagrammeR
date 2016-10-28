07fd0a3df21f85f61cb2b626a05e9c7364a2132c
Jon Calder <jonmcalder@gmail.com>
2016-10-28 17:41:58 +0200

    Add basic graphviz_template file and source it via initLesson, make a (very rough) start on GraphViz lesson.

--- Graphviz/initLesson.R
+++ Graphviz/initLesson.R
@@ -2,3 +2,12 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
+
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+source(file.path(.get_course_path(), "Diagrammer", "Graphviz", 
+                 "graphviz_template.R"))
