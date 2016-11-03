ae010456ef860b227d470ab1aa0268a43db47b09
Jon Calder <jonmcalder@gmail.com>
2016-11-03 13:25:17 +0200

    Update Mermaid lesson to improve existing example, add another example and extend.

--- Mermaid/initLesson.R
+++ Mermaid/initLesson.R
@@ -3,4 +3,19 @@
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
 
-ABCD <- "graph LR;A-->B;A-->C;B-->D;C-->D"
+LR_ABCD <- c("mermaid('graph LR;A-->B;A-->C;B-->D;C-->D')",
+             "mermaid('graph LR; A-->B; A-->C; B-->D; C-->D')",
+             "mermaid('graph LR;A --> B;A --> C;B --> D;C --> D')",
+             "mermaid('graph LR; A --> B; A --> C; B --> D; C --> D')"
+             )
+TB_ABCD <- c("mermaid('graph TB;A---B;A---C;B---D;C---D')",
+             "mermaid('graph TB; A---B; A---C; B---D; C---D')",
+             "mermaid('graph TB;A --- B;A --- C;B --- D;C --- D')",
+             "mermaid('graph TB; A --- B; A --- C; B --- D; C --- D')"
+            )
+             
+RL_ABCD <- c("mermaid('graph RL;A---B;B---C;B-->D')",
+             "mermaid('graph RL; A---B; B---C; B-->D')",
+             "mermaid('graph RL;A --- B;B --- C;B --> D')",
+             "mermaid('graph RL; A --- B; B --- C; B --> D')"
+            )
