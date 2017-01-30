37d485365e88e86de3828ccc73553c7b2d90bb36
jonmcalder <jonmcalder@gmail.com>
2017-01-30 22:22:01 +0200

    Correct last added question, update lesson with script question.

--- Node_and_Edge_Data_Frames/customTests.R
+++ Node_and_Edge_Data_Frames/customTests.R
@@ -10,3 +10,14 @@
       # variables when appropriate. The answer test, creates_new_var()
       # can be used for for the purpose, but it also re-evaluates the
       # expression which the user entered, so care must be taken.
+
+test_node_attributes <- function() {
+  try({
+    t1 <- identical(graph_x_ndf$shape, rep("circle", 10))
+    t2 <- identical(graph_x_ndf$color, rep("red", 10))
+    t3 <- identical(graph_x_ndf$fillcolor, rep("pink", 10))
+    t4 <- identical(graph_x_ndf$fontsize, rep(15, 10))
+    ok <- all(t1, t2, t3, t4)
+  }, silent = TRUE)
+  exists('ok') && isTRUE(ok)
+}
---
+++ Node_and_Edge_Data_Frames/scripts/node_attributes-correct.R
@@ -0,0 +1,33 @@
+# Below is a list of all the node attribute columns that will be automatically
+# recognized with a node data frame in DiagrammeR.
+# 
+# color — provide an X11 or hexadecimal color (append 2 digits to hex for alpha)
+# distortion — the node distortion for any shape = polygon
+# fillcolor — provide an X11 or hexadecimal color (append 2 digits to hex for alpha)
+# fixedsize — true or false
+# fontcolor — provide an X11 or hexadecimal color (append 2 digits to hex for alpha)
+# fontname — the name of the font
+# fontsize — the size of the font for the node label
+# height — the height of the node
+# penwidth — the thickness of the stroke for the shape
+# peripheries — the number of peripheries (essentially, additional shape outlines)
+# shape — the node shape (e.g., ellipse, polygon, circle, etc.)
+# sides — if shape = polygon, the number of sides can be provided here
+# style — usually given the value filled if you'd like to fill a node with a color
+# tooltip — provide text here for an unstyled browser tooltip
+# width — the width of the node
+# x — the x position of the node (requires graph attr layout = neato to use)
+# y — the y position of the node (requires graph attr layout = neato to use)
+
+# Update the code below to add columns which will inform the following node 
+# attributes:
+# 
+# - nodes should be circles
+# - nodes should have a red color and pink fill color
+# - the font size for the node labels should be 15
+# 
+# Note that all non-numeric node attributes should be specified as string values
+
+graph_x_ndf <- create_node_df(n = 10, type = "letter", label = letters[1:10],
+                              shape = "circle", color = "red", fillcolor = "pink",
+                              fontsize = 15)
